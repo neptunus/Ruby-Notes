ad0d99bc0afe929cd40e900bf51ff9f6840af9ea
Neptúnus <n@ptun.us>
2024-03-15 07:13:13 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const annotatedJson = (fragment) => {
+  const [string, annotation] = fragment
+    .split(/\||・/); // split on | or ・ characters
+  return {
+    string,
+    annotation
+  };
+}
+
+const State = {
+  value: '',
+  handleInput(input) { 
+    this.value = input
+  },
+  get output() {
+    return this.value
+      .split(/{|}|「|」/)
+      .map(annotatedJson)
+      .map(({string, annotation}) => 
+        annotation 
+          ? m('ruby', [m('span', string), m('rt', annotation)])
+          : string    
+      );
+  },
+  get debug() {
+    return this.value
+      .split(/{|}|「|」/)
+      .map(annotatedJson)
+  },
+}
+
+const stringify = (json) => JSON.stringify(json, null, ' ');
+
+const App = {
+  oninit: () => State.value = '「私・わたし」のシャツは「赤・あか」です: Skyrtan {mín|meen} er rauð: Skyrtan {mín|meen} er rauð',
+  view: () => [
+    m(
+      'input[type=text]', {
+        value: State.value, 
+        oninput: (event) => State.handleInput(event.target.value)
+      },
+    ),
+    m('p', State.output),
+    // m('pre', stringify(State.debug)), // debug: show data structure
+  ]
+}
+
+m.mount(document.body, App)
